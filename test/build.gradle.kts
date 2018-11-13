23dd0dcc7561b56755b7401321625374e2d3dc10
Matthias Fax <matthias.fax@gmail.com>
2018-11-13 14:44:35 +0100

    Bump assertk to version 0.13.4

--- test/build.gradle.kts
+++ test/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
     compile(kotlin("stdlib"))
     compile(project(":library"))
     kapt(project(":processor"))
-    testCompile("com.github.matfax.assertk3:assertk-jvm:0.13.1")
+    testCompile("com.github.matfax.assertk3:assertk-jvm:0.13.4")
     testCompile("org.junit.jupiter:junit-jupiter-api:5.3.1")
     testCompile("org.junit.jupiter:junit-jupiter-params:5.3.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")

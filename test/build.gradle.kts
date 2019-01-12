0f51140fdad75fd2ab83aa9815eb0be63dc7c84b
matfax <matthias.fax@gmail.com>
2019-01-12 23:10:30 +0100

    chore: bump dependency versions

--- test/build.gradle.kts
+++ test/build.gradle.kts
@@ -7,10 +7,10 @@ dependencies {
     compile(kotlin("stdlib"))
     compile(project(":library"))
     kapt(project(":processor"))
-    testCompile("com.github.matfax.assertk3:assertk-jvm:0.13.4")
-    testCompile("org.junit.jupiter:junit-jupiter-api:5.3.1")
-    testCompile("org.junit.jupiter:junit-jupiter-params:5.3.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+    testCompile("com.github.matfax.assertk3:assertk-jvm:0.13.5")
+    testCompile("org.junit.jupiter:junit-jupiter-api:5.3.2")
+    testCompile("org.junit.jupiter:junit-jupiter-params:5.3.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.2")
 }
 
 tasks.withType<Test> {

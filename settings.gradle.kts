f80a2dece94133f4d94e64e79a675ddbc82a28d9
matfax <matthias.fax@gmail.com>
2020-10-30 17:01:06 +0100

    build(deps): migrate to gradle enterprise

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,4 +8,16 @@ pluginManagement {
 
 rootProject.name = "klassindex"
 
+plugins {
+    id("com.gradle.enterprise").version("3.5")
+}
+
 include("library", "processor", "test")
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishOnFailure()
+    }
+}

2e69f18af0a6276d5fdfde35a736e746242267e0
Matthias Fax <matthias.fax@gmail.com>
2024-10-17 17:46:28 +0000

    feat: mark generated classes with proper annotation

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation(project(":library"))
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
     implementation("com.squareup:kotlinpoet:1.18.1")
     implementation("com.google.auto.service:auto-service:1.1.1")
     kapt("com.google.auto.service:auto-service:1.1.1")
